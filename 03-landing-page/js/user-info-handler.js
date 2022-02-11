5f5ff76449d666e1a7ee6f597b5774caa8d8109f
Alphonzo Escolar <phonz.dev@gmail.com>
2022-02-11 22:52:52 +0800

    Refactor #toCapitalize, created #getFirstName

--- 03-landing-page/js/user-info-handler.js
+++ 03-landing-page/js/user-info-handler.js
@@ -7,15 +7,15 @@ const userEmail = userInfo.get("userEmail");
 const welcomeTitle = document.querySelector(".welcome__title");
 const welcomeEmail = document.querySelector(".welcome__email");
 
-welcomeTitle.textContent += ` ${toCapitalize(userName)}!`;
+const firstName = getFirstName(userName);
+
+welcomeTitle.textContent += ` ${toCapitalize(firstName)}!`;
 welcomeEmail.textContent += userEmail;
 
+function getFirstName(name) {
+	return name.split(" ")[0];
+}
+
 function toCapitalize(str) {
-	const parts = str.split(" ");
-
-	const newParts = parts.map((part) => {
-		return part[0].toUpperCase() + part.slice(1).toLowerCase();
-	});
-
-	return newParts.join(" ");
+	return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
