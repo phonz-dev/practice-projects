7635d61b9c3b357a87a7cc91981a5dcdd23f672c
Alphonzo Escolar <phonz.dev@gmail.com>
2022-02-21 16:52:54 +0800

    Add displaying of today's main focus feature

--- 04-momentum-app/js/index.js
+++ 04-momentum-app/js/index.js
@@ -1,5 +1,17 @@
 const clock = document.querySelector(".greeting__clock");
 const dayPart = document.querySelector(".day-part");
+const mainFocusPrompt = document.querySelector(".focus__prompt");
+const mainFocus = document.querySelector(".focus__input");
+
+mainFocus.addEventListener("keypress", (event) => {
+	const focusItem = document.querySelector(".focus__item");
+	const todaysFocus = document.querySelector(".focus__item-label");
+	if (event.key === "Enter") {
+		todaysFocus.textContent = mainFocus.value;
+		mainFocusPrompt.style.display = "none";
+		focusItem.style.display = "flex";
+	}
+});
 
 const displayTime = () => {
 	let now = new Date();
