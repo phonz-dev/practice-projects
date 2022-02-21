eb1823823c7b0733d740a83b4036d6e61a607e31
Alphonzo Escolar <phonz.dev@gmail.com>
2022-02-21 15:01:17 +0800

    Add live clock feature

---
+++ 04-momentum-app/js/index.js
@@ -0,0 +1,36 @@
+const clock = document.querySelector(".greeting__clock");
+const dayPart = document.querySelector(".day-part");
+
+const displayTime = () => {
+	let now = new Date();
+	let hour = now.getHours();
+	let minute = now.getMinutes();
+
+	// Set part of the day
+	displayDayPart(hour);
+
+	// Set to 12-hour format
+	hour = hour > 12 ? hour - 12 : hour;
+
+	// Give a nice 2 digit format
+	hour = ("0" + hour).slice(-2);
+	minute = ("0" + minute).slice(-2);
+
+	clock.textContent = `${hour}:${minute}`;
+
+	setTimeout(displayTime, 800);
+};
+
+const displayDayPart = (currentHour) => {
+	const [midDay, lateNoon] = [12, 17];
+
+	if (currentHour < midDay) {
+		dayPart.textContent = "morning";
+	} else if (currentHour < lateNoon) {
+		dayPart.textContent = "afternoon";
+	} else {
+		dayPart.textContent = "evening";
+	}
+};
+
+window.addEventListener("load", displayTime);
