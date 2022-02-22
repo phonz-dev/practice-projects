7b5c8c7463f4c38fa46e207860fe82bbf54718be
Alphonzo Escolar <phonz.dev@gmail.com>
2022-02-22 14:16:39 +0800

    Refactor

---
+++ 04-momentum-app/js/components/globals.js
@@ -0,0 +1,21 @@
+const clock = document.querySelector(".greeting__clock");
+const dayPart = document.querySelector(".day-part");
+
+const mainFocusPrompt = document.querySelector(".focus__prompt");
+const mainFocus = document.querySelector(".focus__input");
+
+const todoBtn = document.querySelector(".footer__todo-btn");
+const todoList = document.querySelector(".footer__todo-list");
+const todoInput = document.querySelector(".footer__todo-input");
+const todoModal = document.querySelector(".footer__todo-modal");
+
+export {
+	clock,
+	dayPart,
+	mainFocusPrompt,
+	mainFocus,
+	todoBtn,
+	todoList,
+	todoModal,
+	todoInput,
+};
--- 04-momentum-app/js/index.js
+++ 04-momentum-app/js/index.js
@@ -1,15 +1,15 @@
+import {
+	clock,
+	dayPart,
+	mainFocusPrompt,
+	mainFocus,
+	todoBtn,
+	todoList,
+	todoModal,
+	todoInput,
+} from "./components/globals.js";
 import { Todo } from "./components/todo.js";
-
-const clock = document.querySelector(".greeting__clock");
-const dayPart = document.querySelector(".day-part");
-
-const mainFocusPrompt = document.querySelector(".focus__prompt");
-const mainFocus = document.querySelector(".focus__input");
-
-const todoBtn = document.querySelector(".footer__todo-btn");
-const todoList = document.querySelector(".footer__todo-list");
-const todoInput = document.querySelector(".footer__todo-input");
-const todoModal = document.querySelector(".footer__todo-modal");
+import { displayTime } from "./components/utilities.js";
 
 todoBtn.addEventListener("click", () => {
 	todoModal.classList.toggle("hide");
@@ -34,37 +34,5 @@ mainFocus.addEventListener("keypress", (event) => {
 		focusItem.style.display = "flex";
 	}
 });
-
-const displayTime = () => {
-	let now = new Date();
-	let hour = now.getHours();
-	let minute = now.getMinutes();
-
-	// Set part of the day
-	displayDayPart(hour);
-
-	// Set to 12-hour format
-	hour = hour > 12 ? hour - 12 : hour;
-
-	// Give a nice 2 digit format
-	hour = ("0" + hour).slice(-2);
-	minute = ("0" + minute).slice(-2);
-
-	clock.textContent = `${hour}:${minute}`;
-
-	setTimeout(displayTime, 800);
-};
-
-const displayDayPart = (currentHour) => {
-	const [midDay, lateNoon] = [12, 17];
-
-	if (currentHour < midDay) {
-		dayPart.textContent = "morning";
-	} else if (currentHour < lateNoon) {
-		dayPart.textContent = "afternoon";
-	} else {
-		dayPart.textContent = "evening";
-	}
-};
 
 window.addEventListener("load", displayTime);
