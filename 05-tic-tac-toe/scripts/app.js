ff9f3b814194f047b8e0034cc38182cb01a16e86
Alphonzo Escolar <phonz.dev@gmail.com>
2022-03-03 20:20:39 +0800

    Refactor

--- 05-tic-tac-toe/scripts/app.js
+++ 05-tic-tac-toe/scripts/app.js
@@ -1,9 +1,13 @@
 import { Board } from "./modules/board.js";
+import { TicTacToe } from "./modules/ticTacToe.js";
 
 const playButton = document.querySelector("#play-btn");
 const markButtonsWrapper = document.querySelector("#mark-btns");
 const markButtons = Array.from(markButtonsWrapper.children);
 const domBoard = document.querySelector(".board");
+
+const board = new Board();
+const ticTacToe = new TicTacToe(board, domBoard, "X", "O");
 
 playButton.addEventListener("click", () => {
 	playButton.classList.toggle("hide");
@@ -17,44 +21,4 @@ markButtons.forEach((button) => {
 	});
 });
 
-class TicTacToe {
-	constructor(board, playerOne, playerTwo) {
-		this.board = board;
-		this.playerOne = playerOne;
-		this.playerTwo = playerTwo;
-		this.currentPlayer = this.playerOne;
-	}
-
-	switchPlayer() {
-		if (this.currentPlayer === this.playerOne) {
-			this.currentPlayer = this.playerTwo;
-		} else {
-			this.currentPlayer = this.playerOne;
-		}
-	}
-}
-
-const playerX = "X";
-const playerO = "O";
-let currentPlayer = playerX;
-
-const switchPlayer = () => {
-	currentPlayer = currentPlayer === playerX ? playerO : playerX;
-};
-
-const tiles = document.querySelectorAll("[data-tile]");
-const board = new Board();
-const tileNumberAndPosition = board.mapIndexToPosition();
-
-tiles.forEach((tile) => {
-	tile.addEventListener("click", () => {
-		if (tile.textContent) return;
-		tile.textContent = currentPlayer;
-		const tileNumber = tile.getAttribute("data-tile");
-		const tilePositon = tileNumberAndPosition[tileNumber];
-		board.placeMark(tilePositon, currentPlayer);
-		switchPlayer();
-		board.recordCurrentState();
-		console.log(board.history);
-	});
-});
+ticTacToe.run();
