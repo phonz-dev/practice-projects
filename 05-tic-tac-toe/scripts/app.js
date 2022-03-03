8bb77cf0b124c8c767579f8565b6262832554b88
Alphonzo Escolar <phonz.dev@gmail.com>
2022-03-03 13:17:43 +0800

    Add DOM board

--- 05-tic-tac-toe/scripts/app.js
+++ 05-tic-tac-toe/scripts/app.js
@@ -1,7 +1,16 @@
 const playButton = document.querySelector("#play-btn");
-const markButtons = document.querySelector("#mark-btns");
+const markButtonsWrapper = document.querySelector("#mark-btns");
+const markButtons = Array.from(markButtonsWrapper.children);
+const domBoard = document.querySelector(".board");
 
 playButton.addEventListener("click", () => {
 	playButton.classList.toggle("hide");
-	markButtons.classList.toggle("hide");
+	markButtonsWrapper.classList.toggle("hide");
+});
+
+markButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		markButtonsWrapper.classList.toggle("hide");
+		domBoard.classList.toggle("hide");
+	});
 });
